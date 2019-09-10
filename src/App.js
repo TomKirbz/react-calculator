c3840eee79d901e16c2d1ee94dc92d5971ea9805
unknown <tomkirby3@gmail.com>
2019-09-11 00:06:07 +0100

    final changes

--- src/App.js
+++ src/App.js
@@ -3,69 +3,102 @@ import logo from './logo.svg';
 import './App.css';
 
 let storedValue = null
+let calcFunction = ""
 
 class App extends React.Component {
-
  state = {
-   value: '',
+   value: 0
  }
- 
 
   produceNumber = (val) => {
-    let currentValue = this.state.value
-    storedValue = currentValue += val
-    this.setState({value: storedValue})
-    console.log(storedValue)
+    if (this.state.value === 0) {
+    let currentValue = val
+    this.setState({value: currentValue})
+    } else if (this.state.value !== 0) {
+    let input = val
+    let newValue = this.state.value.toString() + input
+    this.setState({value: parseInt(newValue)})
+    }
   }
 
-  // equals = () => {
-  //   let newValue = this.state.value
-  //   this.setState({value: newValue})
-  // }
+  equals = () => {
+    if (calcFunction === "+") {
+      let currentValue = this.state.value 
+      let newValue = storedValue + currentValue
+      this.setState({value: newValue})
+
+    } else if (calcFunction === "/") {
+      let currentValue = this.state.value 
+      let newValue = storedValue / currentValue
+      this.setState({value: newValue})
+
+    } else if (calcFunction === "-") {
+      let currentValue = this.state.value 
+      let newValue = storedValue - currentValue
+      this.setState({value: newValue})
+
+    } else if (calcFunction === "x") {
+      let currentValue = this.state.value 
+      let newValue = storedValue * currentValue
+      this.setState({value: newValue})
+    } 
+    
+  }
 
   clearValues = () => {
-    let resetV = this.state.value
-    resetV = ''
-    this.setState({value: resetV})
+    this.setState({value: 0})
+    calcFunction = ""
+    storedValue = null
   }
 
-  // addButton = () => { 
-  //     store = this.state.value
-  //     let newValue = store
-  //     this.setState({value: newValue})
-  //     console.log("add")
-  // }
+  functionButton = (func) => { 
+      storedValue = this.state.value
+      this.setState({value: 0})
+      calcFunction = func
+  }
+
+  backtick = () => {
+    let currentValue = this.state.value
+    if (currentValue !== 0) {
+      currentValue = currentValue.toString();
+      let newValue = currentValue.substring(0, currentValue.length -1);
+      this.setState({value: parseInt(newValue)})
+      } else if (currentValue == 0){
+        this.setState({value: 0})
+      }
+  }
 
   render() {
   return (
     <div>
       <div className="calc-body">
-      <h1 className="value-box" placeholder="0"> {this.state.value} </h1>
+      <h1 className="value-box" placeholder="0"> {this.state.value.toString()} </h1>
       <div className="button-body row1">
-        <div className="clear-value" onClick={this.clearValues}>clear</div>
-        <div className="func">÷</div>
+        <div className="clear-value" onClick={this.clearValues}>Clear</div>
+        <div className="func backtick" onClick={this.backtick}>←</div>
+        <div className="func" onClick={()=> this.functionButton("/")}>÷</div>
       </div>
       <div className="button-body row2">
       <div className="num" onClick={()=> this.produceNumber(7)}>7</div>
       <div className="num" onClick={()=> this.produceNumber(8)}>8</div>
       <div className="num" onClick={()=> this.produceNumber(9)}>9</div>
-      <div className="func">-</div>
+      <div className="func" onClick={()=> this.functionButton("-")}>-</div>
       </div>
       <div className="button-body row3">
       <div className="num" onClick={()=> this.produceNumber(4)}>4</div>
       <div className="num" onClick={()=> this.produceNumber(5)}>5</div>
       <div className="num" onClick={()=> this.produceNumber(6)}>6</div>
-      <div className="func" onClick={()=> this.addButton()}>+</div>
+      <div className="func" onClick={()=> this.functionButton("+")}>+</div>
       </div>
       <div className="button-body row4">
       <div className="num" onClick={()=> this.produceNumber(1)}>1</div>
       <div className="num" onClick={()=> this.produceNumber(2)}>2</div>
       <div className="num" onClick={()=> this.produceNumber(3)}>3</div>
-      <div className="func" onClick={()=> this.produceNumber()}>x</div>
+      <div className="func" onClick={()=> this.functionButton("x")}>x</div>
       </div>
       <div className="button-body row5">
-      <div className="num-0" onClick={()=> this.produceNumber(0)}>0</div>
-      <div className="func">=</div>
+      <div className="num zero" onClick={()=> this.produceNumber(0)}>0</div>
+      <div className="func" onClick={this.equals}>=</div>
       </div>
 
       </div>
@@ -73,11 +106,5 @@ class App extends React.Component {
   );
 }
 }
-
-
-
-/* <button className="my-button" onClick = {this.incrementValue}> Increment </button>
-      <button className="my-button" onClick = {this.decrementValue}> decrement </button> */
-
 
 export default App;
